671bd0787be176560ae1da0442e9a98c0d9c98a7
profxj <xavier@ucolick.org>
2022-01-16 08:45:19 -0800

    FRB table

--- papers/H0_I/Tables/py/tables_H0I.py
+++ papers/H0_I/Tables/py/tables_H0I.py
@@ -11,14 +11,15 @@ from frb.galaxies import nebular
 from frb.galaxies import photom as ph
 
 from zdm.craco import loading
+from zdm import survey
+from zdm import parameters
+from zdm import misc_functions
 
 from IPython import embed
 
 # Local
 sys.path.append(os.path.abspath("../Analysis/py"))
 #import HST_defs
-
-#def mktab_all_lines -- LateX table listing all measured transitions
 
 def mktab_model_params(outfile='tab_model_params.tex', sub=False):
     # Load up 
@@ -78,7 +79,66 @@ def mktab_model_params(outfile='tab_model_params.tex', sub=False):
     print('Wrote {:s}'.format(outfile))
 
 
+
+def mktab_frbs(outfile='tab_frbs.tex', sub=False):
+
+    state = parameters.State()
+    zDMgrid, zvals,dmvals = misc_functions.get_zdm_grid(
+        state, new=True, plot=False, method='analytic')
+    # Load up the surveys
+    names = ['CRAFT/ICS', 'CRAFT/FE', 'CRAFT/ICS892', 'PKS/Mb']
+    names = ['CRAFT/ICS']
+
+    # Open
+    tbfil = open(outfile, 'w')
+
+    # Header
+    #tbfil.write('\\clearpage\n')
+    tbfil.write('\\begin{table*}\n')
+    tbfil.write('\\centering\n')
+    tbfil.write('\\begin{minipage}{170mm} \n')
+    tbfil.write('\\caption{Fast Radio Bursts Analyzed\\label{tab:frbs}}\n')
+    tbfil.write('\\begin{tabular}{cccccccc}\n')
+    tbfil.write('\\hline \n')
+    tbfil.write('Name & Survey & DM & $\\nu$ & SNR & RA & Dec & $z$ \n')
+    tbfil.write('& & & (pc cm$^{-3}$) & (MHz) & & (J2000) & (J2000) & \n')
+    tbfil.write('\\\\ \n')
+    tbfil.write('\\hline \n')
+
+    # Loop on survey
+    for name in names:
+        isurvey = survey.load_survey(name, state, dmvals)
+        #embed(header='111 of tables')
+        # Loop on FRBs
+        for ss in range(isurvey.NFRB):
+            slin = f'{isurvey.frbs["ID"][ss]}'
+            slin += f'& {name}'
+            slin += f'& {isurvey.frbs["DM"][ss]}'
+            slin += f'& {isurvey.frbs["FBAR"][ss]}'
+            slin += f'& {isurvey.frbs["SNR"][ss]}'
+            slin += f'& {isurvey.frbs["XRa"][ss]}'
+            slin += f'& {isurvey.frbs["XDec"][ss]}'
+            slin += f'& {isurvey.frbs["Z"][ss]}'
+
+            # Write
+            tbfil.write(slin)
+            tbfil.write('\\\\ \n')
+
+    # End
+    tbfil.write('\\hline \n')
+    tbfil.write('\\end{tabular} \n')
+    tbfil.write('\\end{minipage} \n')
+    #tbfil.write('{$^a$}Rest-frame value.  Error is dominated by uncertainty in $n_e$.\\\\ \n')
+    #tbfil.write('{$^b$}Assumes $\\nu=1$GHz, $n_e = 4 \\times 10^{-3} \\cm{-3}$, $z_{\\rm DLA} = 1$, $z_{\\rm source} = 2$.\\\\ \n')
+    tbfil.write('\\end{table*} \n')
+
+    tbfil.close()
+
+    print('Wrote {:s}'.format(outfile))
+
+
 # Command line execution
 if __name__ == '__main__':
 
-    mktab_model_params()
+    #mktab_model_params()
+    mktab_frbs()
