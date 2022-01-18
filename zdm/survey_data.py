b425d85559b9483457f82694fcbf3f74a36ca862
profxj <xaiver@ucolick.org>
2022-01-18 05:40:08 -0800

    test pcosmic

--- zdm/survey_data.py
+++ zdm/survey_data.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass, field, asdict
 from zdm import data_class
 
 @dataclass
-class Frequency(data_class.myDataClass):
+class TimeFrequency(data_class.myDataClass):
     BW: float = field(
         default=0., 
         metadata={'help': "Mean Frequency (observed)", 
@@ -18,6 +18,52 @@ class Frequency(data_class.myDataClass):
                   'unit': 'MHz', 
                   'Notation': '',
                   })
+    TRES: float = field(
+        default=0., 
+        metadata={'help': "Time resolution",
+                  'unit': 'ms', 
+                  'Notation': '',
+                  })
+
+@dataclass
+class Telescope(data_class.myDataClass):
+    DIAM: float = field(
+        default=0., 
+        metadata={'help': "Individual antenna diameter", 
+                  'unit': 'm', 
+                  'Notation': '',
+                  })
+    NBEAMS: int = field(
+        default=0, 
+        metadata={'help': "Number of beams/antennae", 
+                  'unit': '', 
+                  'Notation': '',
+                  })
+    BEAM: str = field(
+        default='', 
+        metadata={'help': "Beam file", 
+                  })
+    THRESH: float = field(
+        default=0., 
+        metadata={'help': "Threshold fluence", 
+                  'unit': 'Jy ms', 
+                  'Notation': '',
+                  })
+    SNRTHRESH: float = field(
+        default=0., 
+        metadata={'help': "S/N threshold", 
+                  'unit': '', 
+                  'Notation': '',
+                  })
+
+@dataclass
+class Observing(data_class.myDataClass):
+    TOBS: float = field(
+        default=0., 
+        metadata={'help': "Total observing time", 
+                  'unit': 'hours', 
+                  'Notation': '',
+                  })
 
 class SurveyData(data_class.myData):
     """ Hold the SurveyData in a convenient
@@ -26,4 +72,6 @@ class SurveyData(data_class.myData):
     """
     def set_dataclasses(self):
 
-        self.frequency = Frequency()
+        self.timefrequency = TimeFrequency()
+        self.observing = Observing()
+        self.telescope = Telescope()
