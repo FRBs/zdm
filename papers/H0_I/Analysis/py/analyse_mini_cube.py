08569fa253aea6705bc9864c193e39c7acdc9c9a
Clancy James <clancy.james@curtin.edu.au>
2022-03-15 09:49:20 +0800

    Updated analysis to make it more portable

--- papers/H0_I/Analysis/py/analyse_mini_cube.py
+++ papers/H0_I/Analysis/py/analyse_mini_cube.py
@@ -1,81 +1,176 @@
-import numpy as np
+"""
+This is a script to produce 'useful' outputs for a cube.
 
+It was originally designed for the real mini cube. However,
+    it should serve as a template for other cube analyses.
+
+Currently, the script has to "know" how many surveys
+    are present, and also know which are localised
+    and which not. This possibly could be changed
+    in the future.
+"""
+
+import numpy as np
 import zdm
 from zdm import analyze_cube as ac
-
 from matplotlib import pyplot as plt
 
 def main():
     
-    data=np.load('craco_mini_cube.npz')
-    for item in data:
-        print(item)
+    data=np.load('real_mini_cube.npz')
     
     lst = data.files
     lldata=data["ll"]
-    print("Dimensions are ",lldata.shape)
     params=data["params"]
     
-    print("Max and min likelihoods are ",np.nanmax(lldata), np.nanmin(lldata))
+    if False:
+        combined1=data["pzDM"]+data["pDM"]
+        combined2=data["pDMz"]+data["pz"]
+        diff=(combined1-combined2)/(combined1+combined2)
+    
+    #reconstructs total pdmz given all the pieces, including unlocalised contributions
+    pDMz=data["P_zDM0"]+data["P_zDM1"]+data["P_zDM2"]+data["P_zDM3"]+data["P_zDM4"]
+    
+    #DM only contribution - however it ignores unlocalised DMs
+    pDMonly=data["pDM"]+data["P_zDM0"]+data["P_zDM4"]
+    
+    #do this over all surveys
+    P_s=data["P_s0"]+data["P_s1"]+data["P_s2"]+data["P_s3"]+data["P_s4"]
+    P_n=data["P_n0"]+data["P_n1"]+data["P_n2"]+data["P_n3"]+data["P_n4"]
+    
+    #labels=['p(N,s,DM,z)','P_n','P(s|DM,z)','p(DM,z)all','p(DM)all','p(z|DM)','p(DM)','p(DM|z)','p(z)']
+    #for datatype in [data["ll"],P_n,P_s,pDMz,pDMonly,data["pzDM"],data["pDM"],data["pDMz"],data["pz"]]:
+    
+    # 1D plots by statistical contribution
+    contributions=[data["ll"],P_n,P_s,pDMz]
+    labels=['p(N,s,DM,z)','P_n','P(s|DM,z)','p(DM,z)']
+    make_1d_plots_by_contribution(data,contributions,labels,prefix="by_contribution")
+    
+    # 1D plots by surveys
+    contributions=[data["P_s0"],data["P_s1"],data["P_s2"],data["P_s3"],data["P_s4"]]
+    labels=["CRAFT/FE","CRAFT/ICS (low)","CRAFT/ICS (mid)","CRAFT/ICS (high)","Parkes/Mb"]
+    make_1d_plots_by_contribution(data,contributions,labels,prefix="by_survey_")
+    
+    
+    param_vals=get_param_values(data)
+    
+    ############## 2D plots for total likelihood - parkes ps only ###########
+    #uvals,ijs,arrays,warrays=ac.get_2D_bayesian_data(lldata)
+    uvals,ijs,arrays,warrays=ac.get_2D_bayesian_data(data["P_s4"])
+    for which,array in enumerate(arrays):
+        savename="parkes_s_"+params[ijs[which][0]]+"_"+params[ijs[which][1]]+".pdf"
+        make_2d_plot(array,params[ijs[which][0]],params[ijs[which][1]],param_vals[ijs[which][0]],param_vals[ijs[which][1]],savename=savename)
+        
+    
+    ############## 2D plots for total likelihood ###########
+    uvals,ijs,arrays,warrays=ac.get_2D_bayesian_data(data["ll"])
+    for which,array in enumerate(arrays):
+        savename="lls_"+params[ijs[which][0]]+"_"+params[ijs[which][1]]+".pdf"
+        make_2d_plot(array,params[ijs[which][0]],params[ijs[which][1]],param_vals[ijs[which][0]],param_vals[ijs[which][1]],savename=savename)
+      
+def get_param_values(data):
+    # gets unique values for each axis
+    param_vals=[]
+    param_list=[data["lEmax"],data["H0"],data["alpha"],data["gamma"],data["sfr_n"],data["lmean"],data["lsigma"]]
+    for col in param_list:
+        unique=np.unique(col)
+        param_vals.append(unique)  
+    return param_vals
+    
+def make_1d_plots_by_contribution(data,contributions,labels,prefix=""):
+    """
+    contributions: list of vectors giving various likelihood terms
+    Labels: lists labels stating what these are
+    """
+    ######################### 1D plots, split by terms ################
     all_uvals=[]
     all_vectors=[]
     all_wvectors=[]
     
     combined=data["pzDM"]+data["pDM"]
-    labels=['p(s,DM,z)','p(DM,z)','p(z|DM)','p(DM)','p(DM|z)','p(z)']
-    for datatype in [data["ll"],combined,data["pzDM"],data["pDM"],data["pDMz"],data["pz"]]:
-        continue
+    
+    # gets 1D Bayesian curves for each contribution
+    for datatype in contributions:
         uvals,vectors,wvectors=ac.get_bayesian_data(datatype)
         all_uvals.append(uvals)
         all_vectors.append(vectors)
         all_wvectors.append(wvectors)
     
+    params=data["params"]
     
+    # gets unique values for each axis
     param_vals=[]
+    param_list=[data["lEmax"],data["H0"],data["alpha"],data["gamma"],data["sfr_n"],data["lmean"],data["lsigma"]]
     for col in [data["lEmax"],data["H0"],data["alpha"],data["gamma"],data["sfr_n"],data["lmean"],data["lsigma"]]:
         unique=np.unique(col)
         param_vals.append(unique)
     
-    for which in np.arange(7):
-        continue
+    # assigns different plotting styles to help distinguish curves
+    linestyles=['-','--','-.',':','-','--','-.',':','-','--','-.',':']
+    for which in np.arange(len(param_list)):
         plt.figure()
         plt.xlabel(params[which])
         plt.ylabel('p('+params[which]+')')
         xvals=param_vals[which]
+        #print("Doing this for parameter ",params[which])
         
         for idata,vectors in enumerate(all_vectors):
             vector=vectors[which]
-            plt.plot(xvals,vector,label=labels[idata])
-        
+            #print(labels[idata]," has values ",vector)
+            plt.plot(xvals,vector,label=labels[idata],linestyle=linestyles[idata])
+            #if idata==4:
+            #    break
+            if idata==1:
+                prod = vector
+            elif idata>1:
+                prod = prod*vector
+        prod /= np.sum(prod)
+        #plt.plot(xvals,prod,label="dumb product")
         plt.legend()
-        plt.savefig(params[which]+".pdf")
+        plt.savefig(prefix+params[which]+".pdf")
         plt.close()
     
+      
+def make_2d_plot(array,xlabel,ylabel,xvals,yvals,savename=None):
+    """
+    Makes 2D plot given an array of probabilities
     
-    uvals,ijs,arrays,warrays=ac.get_2D_bayesian_data(lldata)
+    array: 2D grid of data to plot
     
-    for which,array in enumerate(arrays):
-        plt.figure()
-        plt.xlabel(params[ijs[which][0]])
-        plt.ylabel(params[ijs[which][1]])
-        
-        xvals=param_vals[ijs[which][0]]
-        yvals=param_vals[ijs[which][1]]
-        
-        dx=xvals[-1]-xvals[0]
-        dy=yvals[-1]-yvals[0]
-        aspect=dx/dy
-        
-        extent=[np.min(xvals),np.max(xvals),np.min(yvals),np.max(yvals)]
-        
-        plt.imshow(arrays[which].T,origin='lower',extent=extent,aspect=aspect)
-        plt.xlabel(params[ijs[which][0]])
-        plt.ylabel(params[ijs[which][1]])
-        plt.xticks(rotation=90)
-        cbar=plt.colorbar()
-        cbar.set_label('$p('+params[ijs[which][0]]+','+params[ijs[which][1]]+')$')
-        plt.savefig(params[ijs[which][0]]+"_"+params[ijs[which][1]]+".pdf")
-        plt.close()
+    xlabel: string to label on x axis
+    ylabel: string to label y axis
+    
+    xvals: np array, values of x data
+    yvals: np array, values of y data
+    
+    savename (optional): string to save data under
+    """
+    plt.figure()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    # calculates increment in values
+    dx=xvals[-1]-xvals[0]
+    dy=yvals[-1]-yvals[0]
+    
+    nx=xvals.size
+    ny=yvals.size
+    
+    aspect=dx/dy
+    
+    # the dx/2 etc is so that parameter values line up with the centres of each pixel
+    extent=[np.min(xvals)-dx/2.,np.max(xvals)+dx/2.,np.min(yvals)-dy/2.,np.max(yvals)+dy/2.]
+    
+    plt.imshow(array.T,origin='lower',extent=extent,aspect=aspect)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(rotation=90)
+    cbar=plt.colorbar()
+    cbar.set_label('$p('+xlabel+','+ylabel+')$')
+    if savename is None:
+        savename=xlabel+"_"+ylabel+".pdf"
+    plt.savefig(savename)
+    plt.close()
     
 
 main()
