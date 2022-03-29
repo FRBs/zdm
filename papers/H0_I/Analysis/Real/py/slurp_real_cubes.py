a72d51f48ffb2b88e38757a4fb0bceaed36d4016
profxj <xavier@ucolick.org>
2022-03-29 11:14:28 -0700

    slurp

--- papers/H0_I/Analysis/Real/py/slurp_real_cubes.py
+++ papers/H0_I/Analysis/Real/py/slurp_real_cubes.py
@@ -3,7 +3,7 @@
 from zdm import analyze_cube
 
 
-mini = True
+mini = False
 if mini:
     # Emax
     input_file = 'Cubes/real_mini_cube.json'
@@ -26,3 +26,16 @@ if super_mini:
     analyze_cube.slurp_cube(input_file, prefix, 
                             'Cubes/real_super_mini_cube.npz',
                             nsurveys)
+
+full = True
+if full:
+    # Emax
+    input_file = 'Cubes/real_full_cube.json'
+    prefix = 'Cubes/real_full'
+    nsurveys = 5
+
+    # Run it
+    analyze_cube.slurp_cube(input_file, prefix, 
+                            'Cubes/real_full_cube.npz',
+                            nsurveys)
+    
