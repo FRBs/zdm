1f99cdf44f42f3d92784713b4a158a0a99343147
profxj <xavier@ucolick.org>
2022-06-29 14:36:07 -0700

    fussin about

---
+++ papers/F/Analysis/py/analy_F_I.py
@@ -0,0 +1,10 @@
+from zdm.craco import loading
+
+fiducial_survey = 'CRACO_std_May2022'
+
+def craco_mc_survey_grid():
+    """ Load the defaul MonteCarlo survey+grid for CRACO """
+    survey, grid = loading.survey_and_grid(
+        survey_name=fiducial_survey,
+        NFRB=100, lum_func=2, iFRB=100)
+    return survey, grid
--- papers/H0_I/Figures/py/testing.py
+++
@@ -1,5 +0,0 @@
-from figs_zdm_H0_I import fig_craco_varyF_zDM, fig_craco_varyH0_zDM
-
-fig_craco_varyF_zDM("test.pdf", other_param="H0")
-# fig_craco_varyH0_zDM("test.pdf", other_param="F")
-
