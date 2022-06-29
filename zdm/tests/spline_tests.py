7ace8fedd19717387285494568dc2d6070a12897
Ethan Domingo <etddomin@ucsc.edu>
2022-06-29 09:59:46 -0700

    Adding spline_tests.py

---
+++ zdm/tests/spline_tests.py
@@ -0,0 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from zdm import iteration as it
+from zdm.craco import loading
+
+# 1) Build  a spline w/ 1000 points (10^-6 -> 10^6)
+
+#2a) 100 calls to spev of 1000 (x) values 
+
+#2b) 1 call of 100 x 1000 (x) values
+
+#3) Use scipy instead of spline
+
