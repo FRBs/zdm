d12609e251f3b014dde34ec7f894b8fd2e23434e
Ethan Domingo <etddomin@ucsc.edu>
2022-07-13 12:05:06 -0700

    spline test profile

--- zdm/tests/spline_tests.py
+++ zdm/tests/spline_tests.py
@@ -1,36 +1,51 @@
 import numpy as np
 import mpmath
-from matplotlib import pyplot as plt
-
-from zdm import iteration as it
-from zdm.craco import loading
 
 from scipy import interpolate
 
 gamma = -1.01
 
 # 1) Build  a spline w/ 1000 points (10^-6 -> 10^6)
+
+# check if making avals bigger affects anything ?
 avals = 10**np.linspace(-6, 6., 1000)
-numer = np.array([float(mpmath.gammainc(gamma, a=iEE)) for iEE in avals])
+avals2 = 10**np.linspace(-6, 6., 100000)
+avals3 = 10**np.linspace(-6, 6., 999)
+numer = np.array([float(mpmath.gammainc(gamma, a=iEE)) for iEE in avals]) # expensive
+# np.save and then load from file
+test = np.random.rand(1000)
 
-#2a) 100 calls to spev of 1000 (x) values 
-def calls_100(spline):
+spline = interpolate.splrep(avals, numer)
+
+print(len(numer))
+
+#2a) 100 calls to splev of 1000 (x) values 
+def calls_100():
     for i in range(100):
-        interpolate.splev(avals, ([1],[1],[1]))
+         val = interpolate.splev(avals3, spline)
+         #print(val)
+        
 
 
-calls_100(gamma)
 
 #2b) 1 call of 100 x 1000 (x) values
-def calls_1(spline):
-    pass
+def calls_1():
+    val = interpolate.splev(avals2, spline)
+    # call avals
+    # 
 
+for i in range(100):
+    calls_100()
+    calls_1()
 
 #3) Use scipy instead of spline
+
+# find a new interpolater and try make it run faster than these specific calls (down to 0.10s)
 
 '''
 #TIMEPROFILING
 python -m cProfile -o spline_tests.prof spline_tests.py
+
 
 
 '''
