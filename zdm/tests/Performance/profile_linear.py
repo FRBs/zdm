954092e563ce8bbf1e00cb838be8fb8ebfd06505
profxj <xavier@ucolick.org>
2022-07-26 14:41:33 -0700

    time profiling

---
+++ zdm/tests/Performance/profile_linear.py
@@ -0,0 +1,77 @@
+""" Time profiling """
+
+######
+# first run this to generate surveys and parameter sets, by 
+# setting NewSurveys=True NewGrids=True
+# Then set these to False and run with command line arguments
+# to generate *many* outputs
+#####
+
+import numpy as np
+from matplotlib import pyplot as plt
+import mpmath as mp
+
+from zdm import iteration as it
+from zdm.craco import loading
+from zdm import energetics
+
+from IPython import embed
+
+
+# Default parameters
+pparam = 'H0'
+psurvey = 'CRACO_std_May2022'
+pnFRB = 100
+piFRB = 100
+plum_func = 2 # Spline
+pval = 72.  # H0
+
+# Load up
+isurvey, igrid = loading.survey_and_grid(survey_name=psurvey,
+                                    NFRB=pnFRB,
+                                    iFRB=piFRB,
+                                    lum_func=plum_func)
+
+#pvals = np.linspace(pargs.min, pargs.max, pargs.nstep)
+vparams = {}
+vparams[pparam] = None
+vparams['lC'] = -0.9
+
+vparams[pparam] = pval
+
+
+# C,llC=it.minimise_const_only(vparams,grids,surveys, Verbose=False)
+
+def run_spline():
+    igrid.array_cum_lf=energetics.array_cum_gamma_spline
+    igrid.vector_cum_lf=energetics.vector_cum_gamma_spline
+
+    igrid.update(vparams, ALL=True) 
+    return igrid.fractions.copy()
+
+# ###################
+# Now do linear
+
+def run_linear():
+    igrid.array_cum_lf=energetics.array_cum_gamma_linear
+    igrid.vector_cum_lf=energetics.vector_cum_gamma_linear
+
+    # An example of how to turn on log10
+    igrid.use_log10 = False
+    
+    igrid.update(vparams, ALL=True) 
+
+    return igrid.fractions.copy()
+
+
+
+frac1 = run_spline()
+frac2 = run_linear()
+
+ar1_2_diff = np.setdiff1d(frac1, frac2)
+# doing something wrong bc there's no difference?
+print(ar1_2_diff)
+'''
+#TIMEPROFILING
+python -m cProfile -o time_profile.prof time_profile.py
+'''
