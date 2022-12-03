052c46f6bb54aa4661bb8f24df83546ee1454201
profxj <xaiver@ucolick.org>
2022-12-03 10:28:02 -0800

    now, really

--- zdm/data/Surveys/refactor_surveys.py
+++ zdm/data/Surveys/refactor_surveys.py
@@ -8,10 +8,27 @@ from zdm import survey
 #
 if __name__ == "__main__":
 
-    sdir = os.path.join(resource_filename('zdm', 'data'), 
-                        'Surveys', 'Original')
+    # PKS/Mb
+    survey.refactor_old_survey_file(
+        'PKS/Mb', 'parkes_mb_class_I_and_II.ecsv',
+        clobber=False)
+
+    # CRAFT ICS
+    survey.refactor_old_survey_file(
+        'CRAFT/ICS', 'CRAFT_ICS.ecsv', 
+        clobber=False)
 
     # CRAFT FE
     survey.refactor_old_survey_file(
         'CRAFT/FE', 'CRAFT_class_I_and_II.ecsv', 
-        clobber=False, sdir=sdir)
+        clobber=False)
+
+    # CRAFT 892
+    survey.refactor_old_survey_file(
+        'CRAFT/ICS892', 'CRAFT_ICS_892.ecsv', 
+        clobber=False)
+
+    # CRAFT 1632
+    survey.refactor_old_survey_file(
+        'CRAFT/ICS1632', 'CRAFT_ICS_1632.ecsv', 
+        clobber=False)
