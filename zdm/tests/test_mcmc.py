ecc5e9f3860eabbf2b5aeaf07c96422a682f933e
profxj <xavier@ucolick.org>
2025-10-08 11:43:14 -0700

    test fix

--- zdm/tests/test_mcmc.py
+++ zdm/tests/test_mcmc.py
@@ -12,6 +12,10 @@ Note a modification to allow emcee to work on mac osx
 
 import time
 import numpy as np
+import emcee
+import os
+import multiprocessing as mp
+from multiprocessing import cpu_count
 
 
 def log_prob(theta):
@@ -21,29 +25,25 @@ def log_prob(theta):
             break
     return -0.5 * np.sum(theta**2)
     
-import emcee
 
+def test_mcmc():
+    np.random.seed(42)
+    initial = np.random.randn(32, 5)
+    nwalkers, ndim = initial.shape
+    nsteps = 20
 
-np.random.seed(42)
-initial = np.random.randn(32, 5)
-nwalkers, ndim = initial.shape
-nsteps = 20
+    os.environ["OMP_NUM_THREADS"] = "1"
 
-import os
-os.environ["OMP_NUM_THREADS"] = "1"
+    # this mod is required for running on mac osx
+    Pool = mp.get_context('fork').Pool
 
-import multiprocessing as mp
-# this mod is required for running on mac osx
-Pool = mp.get_context('fork').Pool
-from multiprocessing import cpu_count
+    ncpu = cpu_count()
+    print("{0} CPUs".format(ncpu))
 
-ncpu = cpu_count()
-print("{0} CPUs".format(ncpu))
-
-with Pool() as pool:
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob,pool=pool)
-    start = time.time()
-    sampler.run_mcmc(initial, nsteps, progress=True)
-    end = time.time()
-    serial_time = end - start
-    print("MP took {0:.1f} seconds".format(serial_time))
+    with Pool() as pool:
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob,pool=pool)
+        start = time.time()
+        sampler.run_mcmc(initial, nsteps, progress=True)
+        end = time.time()
+        serial_time = end - start
+        print("MP took {0:.1f} seconds".format(serial_time))
